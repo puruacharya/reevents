66dbf34c317ee2625fde23f6574526598b2b173e
Pururava <pururavaacharya@gmail.com>
2018-09-01 16:24:19 +0530

    1

---
+++ src/features/Event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {Grid} from 'semantic-ui-react'
+ class EventDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Grid.Column width={10}>
+            <h2>Left Column</h2>
+
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default EventDashboard
